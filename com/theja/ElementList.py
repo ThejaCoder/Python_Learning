d867ef24375a2c5b10e69db26434b692935c816c
ThejashanS <Thejashan.Shrisowdhaman@gmail.com>
2023-10-02 18:54:11 -0400

    This is a program to check if A,B,C,D,E, or F is on the list by asking the user if the string in the list.

---
+++ com/theja/ElementList.py
@@ -0,0 +1,10 @@
+#Write a function that checks whether an element occurs in a list.
+user = input("Please enter a letter of the alphabet : ")
+
+list = ["a","b","c","d","e","f"]
+
+if (user in list ):
+    print("That is on the list")
+
+else:
+    print("that is not on the list")
